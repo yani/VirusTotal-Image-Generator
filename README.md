8d299cfe6ea92ab97b2a704e4124a2c4a2c9ae90
Yani <yani.bussens@gmail.com>
2016-10-26 04:51:43 +0200

    Update 1.1

--- README.md
+++ README.md
@@ -4,11 +4,19 @@ Should be pretty straigthforward and give a basic idea of how you can generate y
 
 ### Configuration
 All configuration is located in the image.php file.
-- VirusTotal API key
+- VirusTotal API key !REQUIRED
 - Font
 - Logo
-- Cache time (default: 6hrs)
+- Cache time (default: 3hrs)
+- Cache directory
 - Size
+
+Requires the gd2 PHP library;
+http://php.net/manual/en/image.installation.php
+
+A cache is saved both clientside as serverside.
+The serverside uses a directory while for the clientside the headers are set.
+Both use the same cache time value.
 
 ### Usage
 Can be called with the following request, where the hash is that of the file:
@@ -17,7 +25,7 @@ Can be called with the following request, where the hash is that of the file:
 - image.php?q=[MD5]
 
 ### Example image
-![alt tag](http://i.imgur.com/jqAcK6S.png)
+![Virustotal Result Image](http://i.imgur.com/vLpLjSq.png)
 
 Of this file:
 https://www.virustotal.com/en/file/aa2298a7cb4f22b6301f31a95430ee50d6d5a26d75cb302d78361009605ad327/analysis/1425035432/
--- image.php
+++ image.php
@@ -2,7 +2,7 @@
 /*
  * VirusTotal Image Generator
  * https://github.com/Yanikore/VirusTotal-Image-Generator
- * Version: 1.0.0
+ * Version: 1.1.0
  *
  * created by Yani
  * https://github.com/Yanikore
@@ -12,33 +12,47 @@
  * Configuration
  */
 
-// VirusTotal API key
+// VirusTotal API key [REQUIRED]
 $virusTotalAPI = '';
 
 // Font
-$font = 'res/Gidole-Regular.ttf';
+$font = 'res/ProFont.ttf';
 
 // Logo
 $logo = 'res/logo.png';
 
-// Cache time in seconds (21600 = 6hr)
-$cacheTime = 21600;
+// Cache time in seconds (10800 = 3hrs)
+$cacheTime = 10800;
+
+// Cache dir
+$cacheDir = 'cache';
 
 // Size
-$width  = 500;
-$height = 1240;
+$width  = 600;
+$height = 1240; // Height should only be changed if there are new antivir added to VT
 
 /*
  * Actual code
  */
 
-if(!isset($_GET['q']) || !is_string($_GET['q'])) die();
+//error_reporting(0);
+
+if (!isset($_GET['q']) || !is_string($_GET['q'])) {
+    die();
+}
 $qRes = $_GET['q'];
 
 header('Pragma: public');
 header('Cache-Control: max-age=' . $cacheTime);
 header('Expires: '. gmdate('D, d M Y H:i:s \G\M\T', time() + (int)$cacheTime));
 header('Content-Type: image/png');
+
+// Check if cached image exists
+$cachedImage = $cacheDir . DIRECTORY_SEPARATOR . $qRes . '.png';
+//echo $cachedImage;
+if (file_exists($cachedImage) && (filemtime($cachedImage) > (time() - $cacheTime))) {
+    die(readfile($cachedImage));
+}
 
 // Make image
 $image = imagecreate($width, $height);
@@ -53,8 +67,8 @@ $color['background'] = imagecolorallocate($image, 253, 253, 253);
 $color['infobox']    = imagecolorallocate($image, 247, 247, 247);
 $color['border']     = imagecolorallocate($image, 210, 210, 210);
 $color['black']      = imagecolorallocate($image, 75, 75, 75);
-$color['green']      = imagecolorallocate($image, 0, 240, 0);
-$color['red']        = imagecolorallocate($image, 240, 0, 0);
+$color['green']      = imagecolorallocate($image, 0, 210, 0);
+$color['red']        = imagecolorallocate($image, 210, 0, 0);
 
 // Background and border
 imagefill($image, 0, 0, $color['background']);
@@ -67,18 +81,18 @@ imagecopy($image, $logoImage, ($width - $logoX) / 2, 15, 0, 0, $logoX, $logoY);
 
 // Query VirusTotal
 $ch = curl_init();
-curl_setopt($ch,CURLOPT_URL, 'http://www.virustotal.com/vtapi/v2/file/report');
-curl_setopt($ch,CURLOPT_POST, true);
-curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch,CURLOPT_POSTFIELDS, 'resource=' . $qRes . '&apikey=' . $virusTotalAPI);
+curl_setopt($ch, CURLOPT_URL, 'http://www.virustotal.com/vtapi/v2/file/report');
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, 'resource=' . $qRes . '&apikey=' . $virusTotalAPI);
 $result = curl_exec($ch);
 curl_close($ch);
 
 // JSON the result
-$json = json_decode($result, true);
+$json = @json_decode($result, true);
 
 // Check vor a valid result
-if(!$json || $json['response_code'] != '1'){
+if (!$json || $json['response_code'] != '1') {
     $bbox = imageftbbox(18, 0, $font, 'File not found.');
     $x = $bbox[0] + (imagesx($image) / 2) - ($bbox[4] / 2) - 5;
     $y = $bbox[1] + (imagesy($image) / 2) - ($bbox[5] / 2) - 5;
@@ -92,25 +106,34 @@ if(!$json || $json['response_code'] != '1'){
 imagefilledrectangle($image, 30, $logoY + 40, $width - 30, $logoY + 130, $color['infobox']);
 
 // Add text to the Infobox
-imagefttext($image, 11, 0, 40, $logoY + 60, $color['black'], $font, 'SHA1:  ' . $json['sha1']);
-imagefttext($image, 11, 0, 40, $logoY + 80, $color['black'], $font, 'MD5:  ' . $json['md5']);
-imagefttext($image, 11, 0, 40, $logoY + 100, $color['black'], $font, 'Last Scan:  ' . $json['scan_date']);
+imagefttext($image, 11, 0, 40, $logoY + 60, $color['black'], $font, 'SHA1: ');
+imagefttext($image, 11, 0, 140, $logoY + 60, $color['black'], $font, $json['sha1']);
+imagefttext($image, 11, 0, 40, $logoY + 80, $color['black'], $font, 'MD5:  ');
+imagefttext($image, 11, 0, 140, $logoY + 80, $color['black'], $font, $json['md5']);
+imagefttext($image, 11, 0, 40, $logoY + 100, $color['black'], $font, 'Last Scan:  ');
+imagefttext($image, 11, 0, 140, $logoY + 100, $color['black'], $font, $json['scan_date']);
 imagefttext($image, 11, 0, 40, $logoY + 120, $color['black'], $font, 'Status:  ');
 
 // Colour the scan result
-if($json['positives'] > 0) imagefttext($image, 11, 0, 90, $logoY + 120, $color['red'], $font, $json['positives'] . '/' . $json['total']);
-else imagefttext($image, 11, 0, 90, $logoY + 100, $color['green'], $font, $json['positives'] . '/' . $json['total']);
+if ($json['positives'] > 0) {
+    imagefttext($image, 11, 0, 140, $logoY + 120, $color['red'], $font, $json['positives'] . '/' . $json['total']);
+} else {
+    imagefttext($image, 11, 0, 140, $logoY + 120, $color['green'], $font, $json['positives'] . '/' . $json['total']);
+}
 
 // Alphabetize the scanner names
 ksort($json['scans']);
 
 // Loop trough all scans
 $scanY = $logoY + 165;
-foreach($json['scans'] as $name => $array){
+foreach ($json['scans'] as $name => $array) {
     imagefttext($image, 11, 0, 45, $scanY, $color['black'], $font, $name);
 
-    if($array['detected'] == '1') imagefttext($image, 11, 0, $width / 2, $scanY, $color['red'], $font, $array['result']);
-    else imagefttext($image, 11, 0, $width / 2, $scanY, $color['green'], $font, 'Clean');
+    if ($array['detected'] == '1') {
+        imagefttext($image, 11, 0, $width / 2, $scanY, $color['red'], $font, $array['result']);
+    } else {
+        imagefttext($image, 11, 0, $width / 2, $scanY, $color['green'], $font, 'Clean');
+    }
 
     $scanY = $scanY + 18;
 }
@@ -119,8 +142,28 @@ foreach($json['scans'] as $name => $array){
 $bbox = imageftbbox(11, 0, $font, 'made by Yani');
 imagefttext($image, 11, 0, imagesx($image) - $bbox[2] - 5, $height - 5, $color['border'], $font, 'made by Yani');
 
-// Output the file, and clear the resources
+
+// Start a buffer to cache the image after outputting it
+ob_start(function ($data) {
+    global $cachedImage, $cacheDir, $cacheTime;
+
+    // Delete old cached images
+    foreach (glob($cacheDir . DIRECTORY_SEPARATOR . "*") as $imageFile) {
+        if (file_exists($imageFile) && (filemtime($imageFile) < (time() - $cacheTime))) {
+            unlink($imageFile);
+        }
+    }
+
+    // Save the new image in our cache
+    @file_put_contents($cachedImage, $data, LOCK_EX);
+
+    // Return back the image to the main buffer
+    return $data;
+});
+
+// Output the file, and clear resources
 imagepng($image);
 imagedestroy($image);
 
-?>
+// Flush output buffer and cache the image
+ob_end_flush();
